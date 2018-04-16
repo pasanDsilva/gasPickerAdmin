617a9b9cd0a6e3bdcb8da26d8434d42aa2d4562d
Pasan De Silva <Pasan@Pasans-MacBook-Pro.local>
2018-04-16 19:13:20 +0530

    Firbase config

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyDv4bNYIZSVZj_13E0wY5De9vhT_WRblfY',
+    authDomain: 'angular-test1pasan.firebaseapp.com',
+    databaseURL: 'https://angular-test1pasan.firebaseio.com',
+    storageBucket: 'angular-test1pasan.appspot.com'
+  };
