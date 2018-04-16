485b976d3c24b25429f27088704cce2593d0087c
Pasan De Silva <Pasan@Pasans-MacBook-Pro.local>
2018-04-16 19:09:34 +0530

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SignComponent } from './sign/sign.component';
+import { MainComponent } from './main/main.component';
+import { AngularFireModule } from 'angularfire2';
+import { firebaseConfig } from '../environments/firebase.config';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignComponent,
+    MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot([
+      { path:'', component : LoginComponent},
+      { path:'signup', component : SignComponent}
+      
+    ]),
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
