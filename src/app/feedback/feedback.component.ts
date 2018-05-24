99e73701a0d7e34e42f2d75892f4d0149485e779
Pasan De Silva <Pasan@Pasans-MacBook-Pro.local>
2018-05-24 22:48:49 +0530

    feedback done

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -1,4 +1,7 @@
+import { AngularFireDatabase,AngularFireList } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
+// import { Observable } from '@firebase/util';
+import { Observable} from 'rxjs/observable';
 
 @Component({
   selector: 'app-feedback',
@@ -6,12 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./feedback.component.css']
 })
 export class FeedbackComponent  {
+    db:AngularFireDatabase;
+    feedref:AngularFireList<any>;
+    feedback$:Observable<any[]>;
+  constructor(db:AngularFireDatabase) {
 
-  constructor() {
+    this.feedref=db.list('/feedbackDetails');
+    this.feedback$=this.feedref.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
 
-    
+
+   }
 
 
+   removeMessage(key:string){
+      this.feedref.remove(key);
    }
 
   
